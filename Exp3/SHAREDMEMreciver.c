6916d320412ae77cb0901d0ec9969525bcf0d4b2
Hawk1509 <jopaulareeckal@gmail.com>
2024-04-15 06:46:32 +0530

    shared memery

---
+++ Exp3/SHAREDMEMreciver.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/shm.h>
+void main()
+{
+	int shmid;
+	void * shmem;
+	shmid=shmget(1127,1024,0666);
+	shmem=shmat(shmid,NULL,0);
+	int n=atoi(shmem);
+	int f=1;
+	for(int i=1;i<=n;i++)
+	{
+		f=f*i;
+	}
+	printf("Factorial of %d : %d",n,f);
+}
