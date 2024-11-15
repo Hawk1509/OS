8bc656d740a80c55df405d22e8db7922f32c6fb3
JosephPaul <129725289+Hawk1509@users.noreply.github.com>
2024-11-15 10:54:00 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -84,41 +84,6 @@ void main()
 		}
 		
 	}
-	/*for(i=0,k=0,num=0;k<n;)
-	{
-		if(p[k].at<=i&&p[k].status==0)
-		{
-			if(idle==1)
-			{
-				d[num].ct=i;
-				num++;
-			}
-			strcpy(d[num].name,p[k].pname);
-			d[num].st=i;
-			d[num].ct=i+p[k].bt;
-			p[k].tt= d[num].ct-p[k].at;
-			p[k].wt= p[k].tt-p[k].bt;
-			//p[k].wt=d[num].st-p[k].at;
-			//p[k].tt=p[k].wt+p[k].bt;
-			i=d[num].ct;
-			p[k].status=1;
-			k++;
-			num++;
-			idle=0;
-			ls++;
-		}
-		else if(idle==0)
-		{
-			strcpy(d[num].name,"idle");
-			d[num].st=i;
-			i++;
-			idle=1;
-		}
-		else
-		{
-			i++;
-		}
-	}*/
 	printf ( " |pname  | arrival time\t | burst time\t | status\t | wait\t | turn") ;
 	for ( i =0; i <n ; i ++)
 	{
